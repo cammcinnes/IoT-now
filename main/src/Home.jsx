d10d331c373e0b7b9fd0356e49a0d94ac87a051a
Cam McInnes <camtmcinnes@gmail.com>
2024-01-24 09:04:12 -0800

    Complete setup with react router dom

---
+++ main/src/Home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <button />
+    </>
+  )
+}
+
+export default Home
--- main/src/main.jsx
+++ main/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
