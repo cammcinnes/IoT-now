50e7306b5b0811b02317a8fc101f62c5ad53b206
Cam McInnes <camtmcinnes@gmail.com>
2024-02-03 10:39:33 -0800

    Made a private place to store api keys

--- main/backend/index.js
+++ main/backend/index.js
@@ -2,10 +2,13 @@ import OpenAI from "openai";
 import express from "express";
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import fetch from 'node-fetch';
+import {config} from './config.js';
 
+const myKey = config.apiKey;
 
 const openai = new OpenAI({
-  apiKey: ''
+  apiKey: myKey,
 });
 
 // create an simple express api that calls the function above
@@ -19,26 +22,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use cors middleware
 app.use(cors());
 
-  app.post('/', async (req, res) => {
-    const {message} = req.body;
-    try {
-        const completion = await openai.chat.completions.create({
-          messages: [{ role: "user", content: `${message}` }],
-          model: "gpt-3.5-turbo",
-          max_tokens: 100,
-          temperature: 0.5,
-        });
-    
-        console.log(completion.choices[0]);
-        res.json({
-          message: completion.choices[0].text,
-        });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-      }
-    });
+// post request to open ai
+app.post('/', async (req, res) => {
+  const {message} = req.body;
+  try {
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: `${message}` }],
+        model: "gpt-3.5-turbo",
+        max_tokens: 100,
+        temperature: 0.5,
+      });
+  
+      console.log(completion.choices[0]);
+      res.json({
+        message: completion.choices[0].text,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
